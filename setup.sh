dd5b959ee93d026c3529a0bf6563e0ca1bbd237b
Dilunika <kasun.dilunika@contactenergy.co.nz>
2017-06-09 13:35:30 +1200

    Changed the names of the containers.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-sed -i '' 's/ms-nodebootstrap-example/ms-user-microservice/g' docker-compose.yml 
-sed -i '' 's/ms-nodebootstrap-example/ms-user-microservice/g' package.json 
 rm -rf node_modules
+npm install
 npm run build
